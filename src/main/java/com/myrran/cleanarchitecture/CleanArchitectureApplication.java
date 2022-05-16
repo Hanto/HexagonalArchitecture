6768b2a1ec9143465f5186f8db7a75dffcfc1af2
jhanto <jhanto@gmail.com>
2022-05-16 21:45:01 +0200

    First commit

---
+++ src/main/java/com/myrran/cleanarchitecture/CleanArchitectureApplication.java
@@ -0,0 +1,15 @@
+package com.myrran.cleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CleanArchitectureApplication.class, args);
+	}
+
+}
