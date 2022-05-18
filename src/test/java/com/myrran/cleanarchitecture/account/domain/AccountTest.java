a8e324317819941c024177a70e9e8ff27ec71489
jhanto <jhanto@gmail.com>
2022-05-19 00:13:06 +0200

    refactor

--- src/test/java/com/myrran/cleanarchitecture/account/domain/AccountTest.java
+++ src/test/java/com/myrran/cleanarchitecture/account/domain/AccountTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AccountTest
+class AccountTest
 {
     @Nested @DisplayName("WHEN: giving money")
     class Give
--- src/test/java/com/myrran/cleanarchitecture/common/HashserTest.java
+++ src/test/java/com/myrran/cleanarchitecture/common/HashserTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class HashserTest
+class HashserTest
 {
     @Test
     public void orderIndependentHashTest()
