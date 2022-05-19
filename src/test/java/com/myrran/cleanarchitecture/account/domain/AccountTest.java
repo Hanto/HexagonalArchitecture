f28e75943b1ba61485b7e594cb8f4a40ad1af64d
jhanto <jhanto@gmail.com>
2022-05-19 13:04:12 +0200

    refactor

--- src/test/java/com/myrran/cleanarchitecture/account/domain/AccountTest.java
+++ src/test/java/com/myrran/cleanarchitecture/account/domain/AccountTest.java
@@ -19,19 +19,56 @@ class AccountTest
             class EnoughFunds
             {
                 @Test @DisplayName("THEN: success")
-                void enoughFundsCanGiveMoneyToAnotherAccount()
+                void thenSuccess()
                 {
-                    Account account = AccountBuilder.builder()
+                    Money money         = Money.of(10);
+                    AccountId target    = new AccountId(2L);
+                    Account source      = AccountBuilder.builder()
                         .accountId(new AccountId(1L))
                         .balance(Money.of(100))
                         .lastActivities(new LastActivities())
                         .build();
 
-                    boolean success = account.giveTo(Money.of(10), new AccountId(2L));
+                    boolean success = source.giveTo(money, target);
 
                     assertThat(success).isTrue();
-                    assertThat(account.getLastActivities().getActivities()).hasSize(1);
-                    assertThat(account.getBalance()).isEqualTo(Money.of(90));
+                }
+
+                @Test @DisplayName("THEN: activiy added")
+                void thenActivyAdded()
+                {
+                    Money money         = Money.of(10);
+                    AccountId target    = new AccountId(2L);
+                    Account source      = AccountBuilder.builder()
+                        .accountId(new AccountId(1L))
+                        .balance(Money.of(100))
+                        .lastActivities(new LastActivities())
+                        .build();
+
+                    boolean success = source.giveTo(money, target);
+                    Activity activity = source.getLastActivities().getActivities().get(0);
+
+                    assertThat(source.getLastActivities().getActivities()).hasSize(1);
+                    assertThat(activity.getOwnedAccountId()).isEqualTo(source.getAccountId());
+                    assertThat(activity.getSourceAccountId()).isEqualTo(source.getAccountId());
+                    assertThat(activity.getTargetAccountId()).isEqualTo(target);
+                    assertThat(activity.getMoney()).isEqualTo(money.negative());
+                }
+
+                @Test @DisplayName("THEN: balance updated")
+                void balanceUpdated()
+                {
+                    Money money         = Money.of(10);
+                    AccountId target    = new AccountId(2L);
+                    Account source      = AccountBuilder.builder()
+                        .accountId(new AccountId(1L))
+                        .balance(Money.of(100))
+                        .lastActivities(new LastActivities())
+                        .build();
+
+                    boolean success = source.giveTo(money, target);
+
+                    assertThat(source.getBalance()).isEqualTo(Money.of(90));
                 }
             }
 
@@ -39,19 +76,51 @@ class AccountTest
             class NotEnoghtFunds
             {
                 @Test @DisplayName("THEN: failure")
-                void notEnoughFundsCannotGiveMoneyToAnotherAccount()
+                void thenFailure()
                 {
-                    Account account = AccountBuilder.builder()
+                    Money money         = Money.of(110);
+                    AccountId target    = new AccountId(2L);
+                    Account source      = AccountBuilder.builder()
                         .accountId(new AccountId(1L))
-                        .balance(Money.of(9))
+                        .balance(Money.of(100))
                         .lastActivities(new LastActivities())
                         .build();
 
-                    boolean success = account.giveTo(Money.of(10), new AccountId(2L));
+                    boolean success = source.giveTo(money, target);
 
                     assertThat(success).isFalse();
-                    assertThat(account.getLastActivities().getActivities()).hasSize(0);
-                    assertThat(account.getBalance()).isEqualTo(Money.of(9));
+                }
+
+                @Test @DisplayName("THEN: activity not added")
+                void thenActivityNotAdded()
+                {
+                    Money money         = Money.of(110);
+                    AccountId target    = new AccountId(2L);
+                    Account source      = AccountBuilder.builder()
+                        .accountId(new AccountId(1L))
+                        .balance(Money.of(100))
+                        .lastActivities(new LastActivities())
+                        .build();
+
+                    boolean success = source.giveTo(money, target);
+
+                    assertThat(source.getLastActivities().getActivities()).hasSize(0);
+                }
+
+                @Test @DisplayName("THEN: balance not updated")
+                void balancedNotUpdated()
+                {
+                    Money money         = Money.of(110);
+                    AccountId target    = new AccountId(2L);
+                    Account source      = AccountBuilder.builder()
+                        .accountId(new AccountId(1L))
+                        .balance(Money.of(100))
+                        .lastActivities(new LastActivities())
+                        .build();
+
+                    boolean success = source.giveTo(money, target);
+
+                    assertThat(source.getBalance()).isEqualTo(Money.of(100));
                 }
             }
         }
@@ -60,7 +129,7 @@ class AccountTest
         class Same
         {
             @Test @DisplayName("THEN: failure")
-            void cannotGiveMoneyToSameAccount()
+            void thenFailure()
             {
                 Account account = AccountBuilder.builder()
                     .accountId(new AccountId(1L))
@@ -68,10 +137,36 @@ class AccountTest
                     .lastActivities(new LastActivities())
                     .build();
 
-                boolean success = account.giveTo(Money.of(10), new AccountId(1L));
+                boolean success = account.giveTo(Money.of(10), account.getAccountId());
 
                 assertThat(success).isFalse();
+            }
+
+            @Test @DisplayName("THEN: activity not added")
+            void thenActivityNotAdded()
+            {
+                Account account = AccountBuilder.builder()
+                    .accountId(new AccountId(1L))
+                    .balance(Money.of(100))
+                    .lastActivities(new LastActivities())
+                    .build();
+
+                boolean success = account.giveTo(Money.of(10), account.getAccountId());
+
                 assertThat(account.getLastActivities().getActivities()).hasSize(0);
+            }
+
+            @Test @DisplayName("THEN: balance not updated")
+            void balancedNotUpdated()
+            {
+                Account account = AccountBuilder.builder()
+                    .accountId(new AccountId(1L))
+                    .balance(Money.of(100))
+                    .lastActivities(new LastActivities())
+                    .build();
+
+                boolean success = account.giveTo(Money.of(10), account.getAccountId());
+
                 assertThat(account.getBalance()).isEqualTo(Money.of(100));
             }
         }
