78fb178d3004ceb38772670812ccbc628c940fed
jhanto <jhanto@gmail.com>
2022-05-19 12:41:36 +0200

    refactor

--- src/main/java/com/myrran/cleanarchitecture/account/application/AccountService.java
+++ src/main/java/com/myrran/cleanarchitecture/account/application/AccountService.java
@@ -6,12 +6,10 @@ import com.myrran.cleanarchitecture.account.domain.Account;
 import com.myrran.cleanarchitecture.account.domain.AccountId;
 import com.myrran.cleanarchitecture.account.domain.Money;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
 
-@Component
 @RequiredArgsConstructor
 public class AccountService implements AccountServiceI
 {
@@ -20,9 +18,9 @@ public class AccountService implements AccountServiceI
     @Override @Transactional
     public boolean giveMoney(long sourceAccountId, long targetAccountId, BigDecimal quantity)
     {
-        Account sourceAccount = accountDAO.loadAccount(new AccountId(sourceAccountId));
-        Account targetAccount = accountDAO.loadAccount(new AccountId(targetAccountId));
-        Money money = new Money(quantity);
+        Account sourceAccount   = accountDAO.loadAccount(new AccountId(sourceAccountId));
+        Account targetAccount   = accountDAO.loadAccount(new AccountId(targetAccountId));
+        Money money             = new Money(quantity);
 
         if (!sourceAccount.giveTo(money, targetAccount.getAccountId()))
             return false;
--- src/main/java/com/myrran/cleanarchitecture/configuration/SpringBeans.java
+++ src/main/java/com/myrran/cleanarchitecture/configuration/SpringBeans.java
@@ -1,5 +1,8 @@
 package com.myrran.cleanarchitecture.configuration;// Created by jhant on 17/05/2022.
 
+import com.myrran.cleanarchitecture.account.application.AccountService;
+import com.myrran.cleanarchitecture.account.application.ports.AccountDAO;
+import com.myrran.cleanarchitecture.account.application.ports.AccountServiceI;
 import com.myrran.cleanarchitecture.common.Hasher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,4 +13,8 @@ public class SpringBeans
     @Bean
     public Hasher getHasher()
     {   return new Hasher(); }
+
+    @Bean
+    public AccountServiceI getAccountService(AccountDAO accountDAO)
+    {   return new AccountService(accountDAO); }
 }
